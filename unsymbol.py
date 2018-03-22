902576bed9e8fa521ce0a02681767074f8129523
Toni <toni.giorgino@gmail.com>
2018-03-22 15:29:51 +0100

    nn

---
+++ unsymbol.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import sys
+import pandas as pd
+
+
+sl=pd.read_table("SYMBOL.TXT",comment="#",header=None)
+
+
+def convert_symbol_tag(t):
+    tc = t.text.encode("latin-1")
+    so = ""
+    for s in tc:
+        sh = f'0x{s:X}'
+        uh = sl[sl[0]==sh][1].iloc[0] # 0xAAAA
+        uhr = uh.replace("0x","&#x")
+        so += uhr
+    t.attrs.pop("face",None)
+    t.attrs.pop("FACE",None)
+    try:
+        t.string.replace_with(so)
+    except:
+        print(f"Weirdness Replacing {tc} -> {so}")
+        pass
+        
+    
+
+for fn in sys.argv[1:]:
+    with open(fn,"rb") as f:
+        s=BeautifulSoup(f)
+
+    tl=s.find_all("font",face="SYMBOL")
+    for t in tl:
+        convert_symbol_tag(t)
+
+    tl=s.find_all("font",face="symbol")
+    for t in tl:
+        convert_symbol_tag(t)
+
+    with open(fn+".u8.html","wb") as f:
+        f.write(s.encode(formatter="html"))
+
+
+    
