5de9a9dd922f5a8dc6fd6c1ba31566e3a6bacbe8
Toni <toni.giorgino@gmail.com>
2023-12-12 17:05:33 +0100

    black

--- bookmarklet/mk-bookmarklet.py
+++ bookmarklet/mk-bookmarklet.py
@@ -5,14 +5,16 @@ import argparse
 
 # s2x=lambda s: int(s,16)
 
+
 def s2x(s):
     try:
-        r=int(s,16)
+        r = int(s, 16)
     except:
-        r=0
+        r = 0
     return r
 
-bookmarklet="""
+
+bookmarklet = """
 javascript:var view = document.defaultView;
 var symboltab = [THE_TABLE]; function convNode(n) { var n1; if (n == null)
 return; if (n.nodeType == 3) { elem = n.parentNode;
@@ -28,27 +30,26 @@ null) { convNode(n1); n1 = n1.nextSibling; } }
 convNode(document); 
 """
 
-if __name__=="__main__":
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--table', default="SYMBOL.txt")
-    parser.add_argument('--reverse',  action=argparse.BooleanOptionalAction)
+    parser.add_argument("--table", default="SYMBOL.txt")
+    parser.add_argument("--reverse", action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
 
-    sl=pd.read_table(args.table,comment="#",header=None)
+    sl = pd.read_table(args.table, comment="#", header=None)
     if not args.reverse:
-        sl.columns=["hSymbol","hUnicode","Unk"]
+        sl.columns = ["hSymbol", "hUnicode", "Unk"]
     else:
-        sl.columns=["hUnicode","hSymbol","Unk"]
+        sl.columns = ["hUnicode", "hSymbol", "Unk"]
 
-    sl["Symbol"]=sl["hSymbol"].map(s2x)
-    sl["Unicode"]=sl["hUnicode"].map(s2x)
+    sl["Symbol"] = sl["hSymbol"].map(s2x)
+    sl["Unicode"] = sl["hUnicode"].map(s2x)
 
     the_table = np.zeros(256, dtype=int)
     for i, d in sl.iterrows():
-        the_table[d["Symbol"]]=d["Unicode"]
+        the_table[d["Symbol"]] = d["Unicode"]
 
-    the_table_as_string=",".join(map(str,the_table))
+    the_table_as_string = ",".join(map(str, the_table))
 
-    print(bookmarklet.replace("THE_TABLE",the_table_as_string))
-
+    print(bookmarklet.replace("THE_TABLE", the_table_as_string))
